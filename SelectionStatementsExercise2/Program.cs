9c99ff64b73ff5b6f2cb4545c20e21cbb17e54f6
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-03-27 15:48:17 -0400

    Switch caseing

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+
+            {
+                case "math":
+                    Console.WriteLine("Yay math so fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("Wow I love history to!");
+                    break;
+                case "science":
+                    Console.WriteLine("Do you you Bill nye?");
+                    break;
+                case "spanish":
+                    Console.WriteLine("Do you like the mexican quisine?");
+                    break;
+                case "gym":
+                    Console.WriteLine("What is your favorite sport?");
+                    break;
+                default:
+                    Console.WriteLine("Whats your favorite thing obout that?");
+                    Console.WriteLine("Im not familiar with that");
+                    Console.ReadLine();
+                    Console.WriteLine("Thats a cool one");
+                    break;
+
+
+
+
+
+
+
+
+
+
+            }
         }
     }
 }
